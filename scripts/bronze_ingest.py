82af396a33dd47150bf75b9d61fe5c0682dcf5b5
chayuthpong31 <chayuthpong31@gmail.com>
2026-02-19 20:51:45 +0700

    Feat: bronze_ingest

---
+++ scripts/bronze_ingest.py
@@ -0,0 +1,112 @@
+from pyspark.sql import SparkSession
+import requests
+import pandas as pd
+from pathlib import Path
+
+def bronze_ingest():
+    # Get Spark Cluster 
+    spark = SparkSession.builder \
+        .appName("Election2026-ETL") \
+        .master("spark://spark-master:7077") \
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.6.0") \
+        .getOrCreate()
+
+    try:
+        print(f"{'='*20} Start Extracting Data {'='*20}")
+        # ======================= Extract info_province data ==========================================
+        response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_province.json")
+        raw_data = response.json()
+
+        data = raw_data["province"]
+        df = pd.DataFrame(data)
+
+        # df.to_csv(BRONZE_DIR / "bronze_info_province.csv",index=False, encoding='utf-8-sig')
+        df.to_csv("/opt/airflow/data/bronze/bronze_info_province.csv")
+        print("Extract info_province successfull ✅")
+
+        # ======================= Extract info_constituency data ==========================================
+        response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_constituency.json")
+        raw_data = response.json()
+
+        df = pd.DataFrame(raw_data)
+        df_zone_exploded = df.explode('zone')
+
+        # df_zone_exploded.to_csv(BRONZE_DIR / "bronze_info_constituency.csv",index=False, encoding='utf-8-sig')
+        df_zone_exploded.to_csv("/opt/airflow/data/bronze/bronze_info_constituency.csv")
+        print("Extract info_constituency successfull ✅")
+
+        # ======================= Extract info_party_overview data ==========================================
+        response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_party_overview.json")
+        raw_data = response.json()
+
+        df = pd.DataFrame(raw_data)
+
+        # df.to_csv(BRONZE_DIR / "bronze_info_party_overview.csv",index=False, encoding='utf-8-sig')
+        df.to_csv("/opt/airflow/data/bronze/bronze_info_party_overview.csv")
+        print("Extract info_party_overview successfull ✅")
+
+        # ======================= Extract info_mp_candidate data ==========================================
+        response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_mp_candidate.json")
+        raw_data = response.json()
+
+        df = pd.DataFrame(raw_data)
+
+        # df.to_csv(BRONZE_DIR / "bronze_info_mp_candidate.csv",index=False, encoding='utf-8-sig')
+        df.to_csv("/opt/airflow/data/bronze/bronze_info_mp_candidate.csv")
+        print("Extract info_mp_candidate successfull ✅")
+
+        # ======================= Extract info_party_candidate data ==========================================
+        response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_party_candidate.json")
+        raw_data = response.json()
+
+        df = pd.DataFrame(raw_data)
+        df_exploded = df.explode('party_list_candidates')
+        candidates_info = df_exploded['party_list_candidates'].apply(pd.Series)
+        df_final = pd.concat([df_exploded['party_no'], candidates_info], axis=1)
+
+        # df_final.to_csv(BRONZE_DIR / "bronze_info_party_candidate.csv",index=False, encoding='utf-8-sig')
+        df_final.to_csv("/opt/airflow/data/bronze/bronze_info_party_candidate.csv")
+        print("Extract info_party_candidate successfull ✅")
+
+        # ======================= Extract stats_cons data ==========================================
+        response = requests.get("https://stats-ectreport69.ect.go.th/data/records/stats_cons.json")
+        raw_data = response.json()
+        data = raw_data["result_province"]
+        df = pd.DataFrame(data)
+
+        # df.to_csv(BRONZE_DIR / "bronze_stats_cons.csv",index=False, encoding='utf-8-sig')
+        df.to_csv("/opt/airflow/data/bronze/bronze_stats_cons.csv")
+        print("Extract stats_cons successfull ✅")
+
+        # ======================= Extract stats_party data ==========================================
+        response = requests.get("https://stats-ectreport69.ect.go.th/data/records/stats_party.json")
+        raw_data = response.json()
+
+        df_party = pd.DataFrame(raw_data).drop(columns=['candidates'], errors='ignore')
+        # df_party.to_csv(BRONZE_DIR / "party_vote_summary.csv", index=False, encoding='utf-8-sig')
+        df_party.to_csv("/opt/airflow/data/bronze/bronze_party_vote_summary.csv")
+        print("Extract party_vote_summary successfull ✅")
+
+        data_with_candidates = [
+            item for item in raw_data 
+            if "candidates" in item and item["candidates"] is not None and len(item["candidates"]) > 0
+        ]
+
+        if data_with_candidates:
+            df_candidates = pd.json_normalize(
+                data_with_candidates, 
+                record_path=['candidates'], 
+                meta=['party_id']
+            )
+            
+            # df_candidates.to_csv(BRONZE_DIR / "candidate_votes_detailed.csv", index=False, encoding='utf-8-sig')
+            df_candidates.to_csv("/opt/airflow/data/bronze/bronze_candidate_votes_detailed.csv")
+            print("Extract candidate_votes_detailed successfull ✅")
+        else:
+            pass
+
+    except Exception as e:
+        print(f"Connect failed: {e}")
+
+if __name__ == "__main__":
+    bronze_ingest()
