ee4b3c2511f7c0aa1cf49d5ddb07a67f8cb1c2b1
chayuthpong31 <chayuthpong31@gmail.com>
2026-02-24 20:15:35 +0700

    Refactor: Chang Raw data as json file

--- scripts/bronze_ingest.py
+++ scripts/bronze_ingest.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession
 import requests
 import pandas as pd
 import logging
+import json
+from pathlib import Path
 
 # Create logger
 logger = logging.getLogger(__name__)
@@ -14,130 +16,92 @@ def bronze_ingest():
         .config("spark.jars.packages", "org.postgresql:postgresql:42.6.0") \
         .getOrCreate()
 
+    BRONZE_PATH = "/data/bronze/bronze_"
+
     try:
         logger.info(f"{'='*20} Start Extracting Data {'='*20}")
         # ======================= Extract info_province data ==========================================
         response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_province.json")
         raw_data = response.json()
 
-        data = raw_data["province"]
-        df = pd.DataFrame(data)
+        target_path = Path(BRONZE_PATH + "info_province.json")
+        target_path.parent.mkdir(parents=True, exist_ok=True)
 
-        df.to_csv("/data/bronze/bronze_info_province.csv", index=False, encoding='utf-8-sig')
+        with open(target_path, 'w', encoding='utf-8') as f:
+            json.dump(raw_data, f, ensure_ascii=False, indent=4)
+
         logger.info("Extract info_province successfull ✅")
 
         # ======================= Extract info_constituency data ==========================================
         response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_constituency.json")
         raw_data = response.json()
 
-        df = pd.DataFrame(raw_data)
-        df_zone_exploded = df.explode('zone')
+        target_path = Path(BRONZE_PATH + "info_constituency.json")
+        target_path.parent.mkdir(parents=True, exist_ok=True)
 
-        df_zone_exploded.to_csv("/data/bronze/bronze_info_constituency.csv", index=False, encoding='utf-8-sig')
+        with open(target_path, 'w', encoding='utf-8') as f:
+            json.dump(raw_data, f, ensure_ascii=False, indent=4)
+
         logger.info("Extract info_constituency successfull ✅")
 
         # ======================= Extract info_party_overview data ==========================================
         response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_party_overview.json")
         raw_data = response.json()
 
-        df = pd.DataFrame(raw_data)
+        target_path = Path(BRONZE_PATH + "info_party_overview.json")
+        target_path.parent.mkdir(parents=True, exist_ok=True)
 
-        df.to_csv("/data/bronze/bronze_info_party_overview.csv", index=False, encoding='utf-8-sig')
+        with open(target_path, 'w', encoding='utf-8') as f:
+            json.dump(raw_data, f, ensure_ascii=False, indent=4)
+
         logger.info("Extract info_party_overview successfull ✅")
 
         # ======================= Extract info_mp_candidate data ==========================================
         response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_mp_candidate.json")
         raw_data = response.json()
 
-        df = pd.DataFrame(raw_data)
+        target_path = Path(BRONZE_PATH + "info_mp_candidate.json")
+        target_path.parent.mkdir(parents=True, exist_ok=True)
 
-        df.to_csv("/data/bronze/bronze_info_mp_candidate.csv", index=False, encoding='utf-8-sig')
+        with open(target_path, 'w', encoding='utf-8') as f:
+            json.dump(raw_data, f, ensure_ascii=False, indent=4)
+
         logger.info("Extract info_mp_candidate successfull ✅")
 
         # ======================= Extract info_party_candidate data ==========================================
         response = requests.get("https://static-ectreport69.ect.go.th/data/data/refs/info_party_candidate.json")
         raw_data = response.json()
 
-        df = pd.DataFrame(raw_data)
-        df_exploded = df.explode('party_list_candidates')
-        candidates_info = df_exploded['party_list_candidates'].apply(pd.Series)
-        df_final = pd.concat([df_exploded['party_no'], candidates_info], axis=1)
+        target_path = Path(BRONZE_PATH + "info_party_candidate.json")
+        target_path.parent.mkdir(parents=True, exist_ok=True)
 
-        df_final.to_csv("/data/bronze/bronze_info_party_candidate.csv", index=False, encoding='utf-8-sig')
+        with open(target_path, 'w', encoding='utf-8') as f:
+            json.dump(raw_data, f, ensure_ascii=False, indent=4)
+
         logger.info("Extract info_party_candidate successfull ✅")
 
         # ======================= Extract stats_cons data ==========================================
         response = requests.get("https://stats-ectreport69.ect.go.th/data/records/stats_cons.json")
         raw_data = response.json()
-        data = raw_data["result_province"]
-        df = pd.DataFrame(data)
 
-        # 1. prov_summary
-        df_prov_summary = pd.json_normalize(data).drop(columns=['result_party', 'constituencies'], errors='ignore')
+        target_path = Path(BRONZE_PATH + "stats_cons.json")
+        target_path.parent.mkdir(parents=True, exist_ok=True)
 
-        # 2. prov_party
-        df_prov_party = pd.json_normalize(
-            data, 
-            record_path=['result_party'], 
-            meta=['prov_id'],
-            record_prefix='party_'
-        )
+        with open(target_path, 'w', encoding='utf-8') as f:
+            json.dump(raw_data, f, ensure_ascii=False, indent=4)
 
-        # 3. district_candidates (explode 2 layer: constituencies -> candidates)
-        df_district_candidates = pd.json_normalize(
-            data,
-            record_path=['constituencies', 'candidates'],
-            meta=['prov_id', ['constituencies', 'cons_id']],
-            errors='ignore'
-        ).rename(columns={'constituencies.cons_id': 'cons_id'})
-
-        # 4. district_party (explode 2 layer: constituencies -> result_party)
-        df_district_party = pd.json_normalize(
-            data,
-            record_path=['constituencies', 'result_party'],
-            meta=['prov_id', ['constituencies', 'cons_id']],
-            record_prefix='party_',
-            errors='ignore'
-        ).rename(columns={'constituencies.cons_id': 'cons_id'})
-
-        
-        df_prov_summary.to_csv("/data/bronze/bronze_province_summary.csv", index=False)
-        logger.info("Extract province_summary successfull ✅")
-
-        df_prov_party.to_csv("/data/bronze/bronze_province_party_results.csv", index=False)
-        logger.info("Extract province_party_results successfull ✅")
-
-        df_district_candidates.to_csv("/data/bronze/bronze_district_candidates_results.csv", index=False)
-        logger.info("Extract district_candidates_results successfull ✅")
-
-        df_district_party.to_csv("/data/bronze/bronze_district_party_results.csv", index=False)
-        logger.info("Extract district_party_results successfull ✅")
+        logger.info("Extract stats_cons successfull ✅")
 
         # ======================= Extract stats_party data ==========================================
         response = requests.get("https://stats-ectreport69.ect.go.th/data/records/stats_party.json")
         raw_data = response.json()
-        data = raw_data["result_party"]
+        target_path = Path(BRONZE_PATH + "stats_party.json")
+        target_path.parent.mkdir(parents=True, exist_ok=True)
 
-        df_party = pd.DataFrame(data).drop(columns=['candidates'], errors='ignore')
+        with open(target_path, 'w', encoding='utf-8') as f:
+            json.dump(raw_data, f, ensure_ascii=False, indent=4)
 
-        df_party.to_csv("/data/bronze/bronze_party_vote_summary.csv", index=False, encoding='utf-8-sig')
-        logger.info("Extract party_vote_summary successfull ✅")
-
-        data_with_candidates = [
-            item for item in data 
-            if "candidates" in item and item["candidates"] is not None and len(item["candidates"]) > 0
-        ]
-
-        if len(data_with_candidates) > 0:
-            df_candidates = pd.json_normalize(
-                data_with_candidates, 
-                record_path=['candidates']
-            )
-            
-            df_candidates.to_csv("/data/bronze/bronze_candidate_votes_detailed.csv", index=False, encoding='utf-8-sig')
-            logger.info("Extract candidate_votes_detailed successfull ✅")
-        else:
-            pass
+        logger.info("Extract stats_party successfull ✅")
 
     except Exception as e:
         logger.info(f"Connect failed: {e}")
--- scripts/silver_transform.py
+++ scripts/silver_transform.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType
 import logging
+import json
+from pyspark.sql import functions as F
 
 # Create logger
 logger = logging.getLogger(__name__)
@@ -15,35 +17,53 @@ def silver_transform():
 
     BRONZE_PATH = "/data/bronze/bronze_"
     SILVER_PATH = "/data/silver/silver_"
-    # ================================ read all files ==================================
 
-    logger.info("Start Reading Files...")
-    province_df = spark.read.csv(BRONZE_PATH + "info_province.csv", header=True, inferSchema=True)
-    constituency_df = spark.read.csv(BRONZE_PATH + "info_constituency.csv", header=True, inferSchema=True)
-    mp_candidate_df = spark.read.csv(BRONZE_PATH + "info_mp_candidate.csv", header=True, inferSchema=True)
-    party_df = spark.read.csv(BRONZE_PATH + "info_party_overview.csv", header=True, inferSchema=True)
-    party_candidate_df = spark.read.csv(BRONZE_PATH + "info_party_candidate.csv", header=True, inferSchema=True)
-    party_vote_summary_df = spark.read.csv(BRONZE_PATH + "party_vote_summary.csv", header=True, inferSchema=True)
-    province_summary_df = spark.read.csv(BRONZE_PATH + "province_summary.csv", header=True, inferSchema=True)
-    province_party_result_df = spark.read.csv(BRONZE_PATH + "province_party_results.csv", header=True, inferSchema=True)
-    candidate_votes_detail_df = spark.read.csv(BRONZE_PATH + "candidate_votes_detailed.csv", header=True, inferSchema=True)
-    district_candidates_result_df = spark.read.csv(BRONZE_PATH + "district_candidates_results.csv", header=True, inferSchema=True)
-    district_party_result_df = spark.read.csv(BRONZE_PATH + "district_party_results.csv", header=True, inferSchema=True)
+    logger.info(f"{'='*20} Start Transform Data {'='*20}")
+
+    # ======================= Transfrom info_province data ==========================================
+    file_path = BRONZE_PATH + "info_province.json"
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        raw_json_data = json.load(f)
+
+    data = raw_json_data["province"]
+    province_df = spark.createDataFrame(data)
+
+    # drop unused column
+    province_df = province_df.drop("eng")
+
+    province_df.toPandas().to_csv(BRONZE_PATH + "info_province.csv", index=False, encoding='utf-8-sig')
+    logger.info("Transform info_province successfull ✅")
 
     # ======================================================================================
 
-    # ================================= Transform =========================================
-    logger.info("Start Transform Data...")
+    # ======================= Transfrom info_constituency data ==========================================
+    file_path = BRONZE_PATH + "info_constituency.json"
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        raw_json_data = json.load(f)
+
+    constituency_df = spark.createDataFrame(raw_json_data)
+    constituency_df_zone_exploded = constituency_df.withColumn("zone", F.explode("zone"))
+
     # Cast column type
     constituency_df = constituency_df.withColumn("registered_vote", constituency_df["registered_vote"].cast(IntegerType()))
+    
+    constituency_df_zone_exploded.toPandas().to_csv(SILVER_PATH + "info_constituency.csv", index=False, encoding='utf-8-sig')
+    logger.info("Transform info_constituency successfull ✅")
+    # ======================================================================================
+
+    # ======================= Transform info_mp_candidate data ==========================================
+    file_path = BRONZE_PATH + "info_mp_candidate.json"
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        raw_json_data = json.load(f)
+
+    mp_candidate_df = spark.createDataFrame(raw_json_data)
+
+    # Cast column type
     mp_candidate_df = mp_candidate_df.withColumn("mp_app_party_id", mp_candidate_df["mp_app_party_id"].cast(IntegerType()))
-    province_party_result_df = province_party_result_df.withColumn("party_party_id", province_party_result_df["party_party_id"].cast(IntegerType()))
-    district_candidates_result_df = district_candidates_result_df.withColumn("party_id", district_candidates_result_df["party_id"].cast(IntegerType()))
-
-    # Drop unuse column
-    party_vote_summary_df = party_vote_summary_df.drop("party_list_count")
-    province_df = province_df.drop("eng")
-
+    
     # Rename Column
     mp_candidate_df = (mp_candidate_df
                         .withColumnRenamed("mp_app_id","mp_candidate_id")
@@ -51,49 +71,105 @@ def silver_transform():
                         .withColumnRenamed("mp_app_party_id","mp_candidate_party_id")
                         .withColumnRenamed("mp_app_name", "mp_candidate_name"))
     
-    party_df = party_df.withColumnRenamed("id","party_id")
-    province_party_result_df = (province_party_result_df
-                                    .withColumnRenamed("party_party_id","party_id")
-                                    .withColumnRenamed("party_party_list_vote","party_list_vote")
-                                    .withColumnRenamed("party_party_cons_votes","party_cons_votes")
-                                    .withColumnRenamed("party_party_list_vote_percent","party_list_vote_percent")
-                                    .withColumnRenamed("party_party_cons_votes_percent","party_cons_votes_percent"))
-    
-    candidate_votes_detail_df = (candidate_votes_detail_df
-                                    .withColumnRenamed("mp_app_id","mp_candidate_id")
-                                    .withColumnRenamed("mp_app_vote","mp_candidate_vote")
-                                    .withColumnRenamed("mp_app_vote_percent","mp_candidate_vote_percent")
-                                    .withColumnRenamed("mp_app_rank","mp_candidate_rank"))
-
-    district_candidates_result_df = (district_candidates_result_df
-                                    .withColumnRenamed("mp_app_id","mp_candidate_id")
-                                    .withColumnRenamed("mp_app_vote","mp_candidate_vote")
-                                    .withColumnRenamed("mp_app_vote_percent","mp_candidate_vote_percent")
-                                    .withColumnRenamed("mp_app_rank","mp_candidate_rank"))
-    
-    district_party_result_df = (district_party_result_df
-                                    .withColumnRenamed("party_party_id","party_id")
-                                    .withColumnRenamed("party_party_list_vote","party_list_vote")
-                                    .withColumnRenamed("party_party_list_vote_percent","party_list_vote_percent"))
-    
-    # ====================================================================================
-
-    # ======================================== Save files =========================================
-    
-    province_df.toPandas().to_csv(SILVER_PATH + "province.csv", index=False)
-    constituency_df.toPandas().to_csv(SILVER_PATH + "constituency.csv", index=False)
     mp_candidate_df.toPandas().to_csv(SILVER_PATH + "mp_candidate.csv", index=False) 
+    logger.info("Transform info_mp_candidate successfull ✅")
+    # ======================================================================================
+
+    # ======================= Transform info_party_overview data ==========================================
+    file_path = BRONZE_PATH + "info_party_overview.json"
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        raw_json_data = json.load(f)
+
+    party_df = spark.createDataFrame(raw_json_data)
+
+    # Rename Column
+    party_df = party_df.withColumnRenamed("id","party_id")
+
     party_df.toPandas().to_csv(SILVER_PATH + "party.csv", index=False) 
-    party_candidate_df.toPandas().to_csv(SILVER_PATH + "party_candidate.csv", index=False)
-    party_vote_summary_df.toPandas().to_csv(SILVER_PATH + "party_vote_summary.csv", index=False)
-    province_summary_df.toPandas().to_csv(SILVER_PATH + "province_summary.csv", index=False)
-    province_party_result_df.toPandas().to_csv(SILVER_PATH + "province_party_result.csv", index=False)
-    candidate_votes_detail_df.toPandas().to_csv(SILVER_PATH + "candidate_votes_detail.csv", index=False)
-    district_candidates_result_df.toPandas().to_csv(SILVER_PATH + "district_candidates_result.csv", index=False)
-    district_party_result_df.toPandas().to_csv(SILVER_PATH + "district_party_result.csv", index=False)
+    logger.info("Transform info_party_overview successfull ✅")
+    # ======================================================================================
 
+    # ======================= Transform info_party_candidate data ==========================================
+    file_path = BRONZE_PATH + "info_party_candidate.json"
+    with open(file_path, "r", encoding="utf-8") as f:
+        raw_json_data = json.load(f)
+
+    df = spark.createDataFrame(raw_json_data)
+
+    df_final = df.withColumn("candidate", F.explode("party_list_candidates")) \
+                .select(
+                    "party_no", 
+                    "candidate.*"  
+                )
+
+    df_final.toPandas().to_csv(SILVER_PATH + "party_candidate.csv", index=False, encoding='utf-8-sig')
+
+    logger.info("Transform info_party_candidate successfull ✅")
+    # ======================================================================================
+
+    # ======================= Transform stats_cons data ==========================================
+    file_path = BRONZE_PATH + "stats_cons.json"
+    df = spark.read.option("multiLine", "true").json(file_path)
+
+    df_raw = df.select(F.explode("result_province").alias("data")).select("data.*")
+
+    df_prov_summary = df_raw.drop("result_party", "constituencies")
+
+    df_prov_party = df_raw.select("prov_id", F.explode("result_party").alias("party")) \
+        .select("prov_id", "party.*") \
+        .withColumn("party_id", F.col("party_id").cast(IntegerType())) \
+        .withColumnRenamed("party_list_vote", "party_list_vote") 
+
+    df_district_candidates = df_raw.select("prov_id", F.explode("constituencies").alias("cons")) \
+        .select("prov_id", F.col("cons.cons_id"), F.explode("cons.candidates").alias("cand")) \
+        .select("prov_id", "cons_id", "cand.*") \
+        .withColumn("party_id", F.col("party_id").cast(IntegerType())) \
+        .withColumnRenamed("mp_app_id", "mp_candidate_id") \
+        .withColumnRenamed("mp_app_vote", "mp_candidate_vote")
+
+    df_district_party = df_raw.select("prov_id", F.explode("constituencies").alias("cons")) \
+        .select("prov_id", F.col("cons.cons_id"), F.explode("cons.result_party").alias("p_res")) \
+        .select("prov_id", "cons_id", "p_res.*") \
+        .withColumnRenamed("party_id", "party_id")
+
+    df_prov_summary.toPandas().to_csv(SILVER_PATH + "province_summary.csv", index=False, encoding='utf-8-sig')
+    df_prov_party.toPandas().to_csv(SILVER_PATH + "province_party_result.csv", index=False, encoding='utf-8-sig')
+    df_district_candidates.toPandas().to_csv(SILVER_PATH + "district_candidates_result.csv", index=False, encoding='utf-8-sig')
+    df_district_party.toPandas().to_csv(SILVER_PATH + "district_party_result.csv", index=False, encoding='utf-8-sig')
+
+    logger.info("All Extract & Transform process successful ✅")
+    # ======================================================================================
+
+    # ======================= Extract stats_party data ==========================================
+    file_path = BRONZE_PATH + "info_party_candidate.json"
+    with open(file_path, "r", encoding="utf-8") as f:
+        raw_json_data = json.load(f)
+
+    data = raw_json_data["result_party"]
+
+    df_raw = spark.createDataFrame(data)
+
+    df_party = df_raw.drop("candidates", "party_list_count")
+
+    df_party.toPandas().to_csv(SILVER_PATH + "party_vote_summary.csv", index=False, encoding='utf-8-sig')
+    logger.info("Extract party_vote_summary successful ✅")
+
+
+    if "candidates" in df_raw.columns:
+        
+        df_candidates = df_raw.filter(F.col("candidates").isNotNull()) \
+                            .withColumn("cand", F.explode("candidates")) \
+                            .select("party_id", "cand.*") 
+
+        df_candidates.toPandas().to_csv(SILVER_PATH + "candidate_votes_detail.csv", index=False, encoding='utf-8-sig')
+        logger.info("Transform candidate_votes_detailed successful ✅")
+    else:
+        pass
+
+    # ======================================================================================
     logger.info("Save files Successfull...")
-    # =============================================================================================
-
+    
+    
 if __name__ == "__main__":
     silver_transform()
